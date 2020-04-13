1795902bb048128a23114fae2c8799970dc36127
landax53 <vincentlandais@aol.fr>
2020-04-13 23:38:53 +0200

    "modif 06_pig_latin.rb"

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,18 +1,8 @@
 def s(str)
-    if str[0].in? ['a', 'e', 'i', 'o', 'u']
-        return str << 'ay'
-    elsif
-        str[0,1].in? ['b', 'c', 'h']
-        first = str.strip[0] # Get the first letter
-        second = str.strip[1] # Get the second letter
-        str = str.sub first, '' # remove the first letter from word
-        str = str.sub second, '' # remove the second letter from word
-        return str << first << second << 'ay'
-    else
-        str[0].in? ['b', 'c', 'h']
-        first = str.strip[0] # Get the first letter
-        str = str.sub first, '' # remove the first letter from word
-        return str << first << 'ay'
-    end
+    vowels = ['a', 'e', 'i', 'o', 'u']
+        if vowels.include? str[0]
+            return str.gsub(/([aeiou])(\w+)*/, '\1\2ay') 
+        else 
+            return str.gsub(/([^aeiou])(\w+)*/, '\2\1ay')
+        end
 end
-puts s("apple")
